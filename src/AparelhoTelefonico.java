c8fe038c65e0b86e0feebf34250c5f18204f37d5
ZATIX\diego.vieira <diego.vieira@omnilink.com.br>
2024-10-11 21:12:06 -0300

    Criado diagrama UML como solicitado, criado interfaces e classe que implementa as interfaces criadas.

---
+++ src/AparelhoTelefonico.java
@@ -0,0 +1,10 @@
+package src;
+
+public interface AparelhoTelefonico{
+
+    public void ligar(String numero);
+    public void atenderChamada();
+    public void desligarChamada();
+    public void iniciarCorreioVoz();
+    
+}
---
+++ src/ReprodutorMusical.java
@@ -0,0 +1,11 @@
+package src;
+
+public interface ReprodutorMusical{
+
+    public void tocar();
+    public void pausar();
+    public void passarMusica();
+    public void voltarMusica();
+    public void selecionarMusica(String musica);
+
+}
